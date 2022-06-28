0cc45d84e27ce204c080b6e2f033c92afea3b4a5
john doe <araizkhan2330@gmail.com>
2022-06-28 23:52:08 +0500

    Changed onlclick() function from html, implemented button click-function whole in JS

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function saveclick()
-{
- console.log("Button Clicked!")
-}
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click",function(){
+ console.log("Button clicked!")
+})
