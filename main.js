71e30ac3b90a37b4c70cd6044801dc9c6c354d5d
john doe <araizkhan2330@gmail.com>
2022-06-29 23:53:15 +0500

    Modified JS to display the saved leads

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-
-let inputBtn = document.getElementById("input-btn")
+let myleads= []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click",function(){
- console.log("Button clicked!")
+ myleads.push(inputEl.value)
+ 
 })
+
+for(let i =0;i<myleads.length;i++)
+{
+ ulEl.innerHTML += "<li>"+myleads[i]+"</li>"
+}
