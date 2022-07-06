5d2394e223804b2defb9b87885273f0a20336163
john doe <araizkhan2330@gmail.com>
2022-07-06 23:07:33 +0500

    Refactoring of code

--- main.js
+++ main.js
@@ -3,42 +3,24 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
-deleteBtn.addEventListener("dblclick",function(){
-  localStorage.clear()
-  myleads = []
-  renderLeads()
-})
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage)
 {
   myleads = leadsFromLocalStorage;
-  renderLeads()
+  render(myleads)
 }
 
-inputBtn.addEventListener("click",function(){
- myleads.push(inputEl.value)
- inputEl.value = ""
 
-localStorage.setItem("myLeads",JSON.stringify(myleads))
-
- renderLeads()
-
- console.log(localStorage.getItem("myLeads"))
-})
-
-
-function renderLeads()
+function render(leads)
 {
  let saveLeads = ""
-for(let i =0;i<myleads.length;i++)
+for(let i =0;i<leads.length;i++)
 {
   saveLeads += `
   <li> 
-      <a target='_blank' href='${myleads[i]}'>
-        ${myleads[i]}
+      <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
       </a>
   </li>
   `
@@ -46,3 +28,22 @@ for(let i =0;i<myleads.length;i++)
 
 ulEl.innerHTML = saveLeads;
 }
+
+
+deleteBtn.addEventListener("dblclick",function(){
+  localStorage.clear()
+  myleads = []
+  render(myleads)
+})
+
+inputBtn.addEventListener("click",function(){
+ myleads.push(inputEl.value)
+ inputEl.value = ""
+
+localStorage.setItem("myLeads",JSON.stringify(myleads))
+
+ render(myleads)
+
+ console.log(localStorage.getItem("myLeads"))
+})
+
