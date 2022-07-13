5a09ffbbf22507f0391a141a6ad3a497c62e9314
john doe <araizkhan2330@gmail.com>
2022-07-13 23:33:38 +0500

    Added save tab button which saves the current chrome tab's link in te extension by just the click of a button and refactoring of previous code

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
 if(leadsFromLocalStorage)
 {
@@ -11,6 +12,14 @@ if(leadsFromLocalStorage)
   render(myLeads)
 }
 
+tabBtn.addEventListener("click",function()
+{
+  chrome.tabs.query({active:true,currentWindow:true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myleads",JSON.stringify(myLeads))
+    render(myLeads)
+  })
+})
 
 function render(leads)
 {
